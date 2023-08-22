e0abb9531acbe7b07f713f24347e2e89934a67ea
Rafael Krstačić <ray.back99@gmail.com>
2023-08-22 17:16:35 +0200

    Dodana ruta About i grupirane rute za web

---
+++ frontend/src/WebWrapper.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="app-web">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WebWrapper",
+};
+</script>
+
+<style lang="scss">
+#app-web {
+    padding: 8em;
+    text-align: left;
+    line-height: 20px;
+
+    h1 {
+        font-size: 4rem;
+    }
+
+    h2 {
+        font-size: 3rem;
+    }
+
+    h3 {
+        font-size: 2.5rem;
+    }
+
+    h4 {
+        font-size: 2rem;
+    }
+
+    h5 {
+        font-size: 1.5rem;
+    }
+
+    h6 {
+        font-size: 1.25rem;
+    }
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,88 +1,101 @@
 import { createRouter, createWebHistory } from "vue-router";
-import WebsiteHome from "../views/WebsiteHomeView.vue";
+import WebsiteHome from "@/views/Web/HomeView.vue";
+import WebWrapper from "@/WebWrapper.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "HomeView",
-    component: WebsiteHome,
-    meta: {
-      authRequired: false,
+    {
+        path: "/",
+        name: "HomeView",
+        component: WebWrapper,
+        meta: {
+            authRequired: false,
+        },
+        children: [
+            {
+                path: "/",
+                name: "HomeView",
+                component: WebsiteHome,
+            },
+            {
+                path: "about",
+                name: "AboutUs",
+                component: () => import("@/views/Web/AboutView"),
+            },
+        ],
     },
-  },
-  {
-    path: "/login",
-    name: "LoginView",
-    meta: {
-      authRequired: false,
+    {
+        path: "/login",
+        name: "LoginView",
+        meta: {
+            authRequired: false,
+        },
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
     },
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
-  },
-  {
-    path: "/error",
-    name: "Error",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Error.vue"),
-  },
-  {
-    path: "/success",
-    name: "Success",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Success.vue"),
-  },
-  {
-    path: "/newsfeed",
-    name: "NewsfeedView",
-    meta: {
-      authRequired: true,
+    {
+        path: "/error",
+        name: "Error",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/Error.vue"),
     },
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/NewsfeedView.vue"),
-  },
+    {
+        path: "/success",
+        name: "Success",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/Success.vue"),
+    },
+    {
+        path: "/newsfeed",
+        name: "NewsfeedView",
+        meta: {
+            authRequired: true,
+        },
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/NewsfeedView.vue"),
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 router.beforeEach((to, _from, next) => {
-  const token = localStorage.getItem("token");
-  const isUserLoggedIn = token !== null;
+    const token = localStorage.getItem("token");
+    const isUserLoggedIn = token !== null;
 
-  // provjera auth
-  if (to.meta.authRequired) {
-    if (isUserLoggedIn) {
-      if (to.name !== "NewsfeedView") {
-        return next({ name: "NewsfeedView" });
-      }
+    // provjera auth
+    if (to.meta.authRequired) {
+        if (isUserLoggedIn) {
+            if (to.name !== "NewsfeedView") {
+                return next({ name: "NewsfeedView" });
+            }
+        } else {
+            if (to.name !== "LoginView") {
+                return next({ name: "LoginView" });
+            }
+        }
     } else {
-      if (to.name !== "LoginView") {
-        return next({ name: "LoginView" });
-      }
+        if (isUserLoggedIn) {
+            if (to.name === "LoginView" || to.name === "HomeView") {
+                return next({ name: "NewsfeedView" });
+            }
+        }
     }
-  } else {
-    if (isUserLoggedIn) {
-      if (to.name === "LoginView" || to.name === "HomeView") {
-        return next({ name: "NewsfeedView" });
-      }
-    }
-  }
 
-  return next();
+    return next();
 });
 
 export default router;
