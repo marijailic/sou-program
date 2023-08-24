13a95656d4838bcd36d55340b4375e9bc475655c
Rafael Krstačić <ray.back99@gmail.com>
2023-08-24 21:32:26 +0200

    Create EmailLink.vue

---
+++ frontend/src/components/Web/EmailLink.vue
@@ -0,0 +1,23 @@
+<template>
+    <a :href="{ mailHref }">{{ mailto }}</a>
+</template>
+
+<script>
+export default {
+    name: "EmailLink",
+    data() {
+        const mailto = process.env.VUE_APP_CONTACT_EMAIL;
+        const subject = "Email from site";
+        const subjcetURIEncoded = encodeURIComponent(subject);
+
+        const mailHref = `mailto:${mailto}?subject=${subjcetURIEncoded}`;
+
+        return {
+            mailto,
+            mailHref,
+        };
+    },
+};
+</script>
+
+<style scoped></style>
