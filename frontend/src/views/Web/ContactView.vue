4604773229786d1176d0b03aaaccdc5cf708ec69
Rafael Krstačić <ray.back99@gmail.com>
2023-08-27 21:32:06 +0200

    Contact & Podcast layout

--- frontend/src/views/Web/ContactView.vue
+++ frontend/src/views/Web/ContactView.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>Contact</div>
+    <div>
+        <div class="row">
+            <div class="col">
+                <h1 id="contact">Kontaktirajte nas na <EmailLink /></h1>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import EmailLink from "@/components/Web/EmailLink.vue";
+
 export default {
     name: "Contact",
+    components: { EmailLink },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+h1#contact {
+    font-size: 5rem;
+    line-height: 1.5;
+}
+</style>
