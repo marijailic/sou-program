c9f64c6de24664ac83852dbd61b41fdfefc8f455
Alan Bubalo <alan.buba5@gmail.com>
2023-11-23 23:31:56 +0100

    Fixes

--- frontend/src/services/imageService.js
+++ frontend/src/services/imageService.js
@@ -2,6 +2,9 @@ import { useStoreGallery } from '@/stores/gallery.store';
 
 export default {
     async getImageSrc(imageID) {
+        if (!imageID) {
+            return '';
+        }
         const storeGallery = useStoreGallery();
         const imageBase64 = await storeGallery.getImage(imageID);
         return imageBase64 ? `data:image/jpeg;base64,${imageBase64}` : '';
